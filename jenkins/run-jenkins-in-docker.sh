6d525ca708045dcdcb1eb2b4ec84cddd6e92d222
Kyle Shen <fogogo@126.com>
2019-03-20 23:30:17 +0800

    Jenkins

--- jenkins/run-jenkins-in-docker.sh
+++ jenkins/run-jenkins-in-docker.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 # Run jenkins in backend.
-sudo docker run \
+docker run \
   -d \
+  --rm \
   -u root \
   -p 8080:8080 \
   -v jenkins-data:/var/jenkins_home \
+  -v /usr/bin/docker:/usr/bin/docker \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$HOME":/home \
   jenkinsci/blueocean
+
+# Jenkins initial password in docker volumes
+cat /var/lib/docker/volumes/jenkins-data/_data/secrets/initialAdminPassword
---
+++ linux/linux.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Ubuntu switch to root user
+sudo su
+
+
+docker ps -a
