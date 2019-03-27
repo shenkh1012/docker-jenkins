f57f6e72748851c5c3dd62538b8c69c93df816c3
Kyle Shen <kyle.shen@delphi-tech.com>
2019-03-27 17:14:25 +0800

    Update jenkins file

--- jenkins/run-jenkins-in-docker.sh
+++ jenkins/run-jenkins-in-docker.sh
@@ -20,6 +20,7 @@ docker run \
   -u root \
   -p 8080:8080 \
   -v jenkins-data:/var/jenkins_home \
+  -v /usr/bin/docker:/usr/bin/docker \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$HOME":/home \
   jenkinsci/blueocean
